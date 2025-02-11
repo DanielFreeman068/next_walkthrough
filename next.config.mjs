da8029c401a82422db0edc1368929ce216f77b1b
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-02-11 14:32:12 -0700

    update on notes for next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images:{
+        remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'ichef.bbci.co.uk',
+            port:'',
+            search:''
+            },
+        ],
+    }
+};
 
 export default nextConfig;
--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const page = () => {
     return (
         <div>
             <h1>hello</h1>
+            <Link href='/'>Home</Link>
+            <Image
+            src='https://ichef.bbci.co.uk/ace/standard/873/cpsprodpb/598a/live/75307200-ae2b-11ee-b4f2-5db09c62f717.jpg'
+            alt='Next.js Logo'
+            width={180}
+            height={38}
+            ></Image>
         </div>
     )
 }
